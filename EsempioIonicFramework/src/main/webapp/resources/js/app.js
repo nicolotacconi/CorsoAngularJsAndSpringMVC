6dca1b4d1f88c4ddb14f8fe9010e9bae442b45fd
Giuseppe <giuseppe.giordano1992@gmail.com>
2015-10-27 09:11:40 +0100

    Eliminato progetto EsempioIonicFramework

--- EsempioIonicFramework/src/main/webapp/resources/js/app.js
+++
@@ -1,5 +0,0 @@
-var ionicAngular = angular.module('ionicAngular', [ 'ionic',
-		'ionicAngular.controllers', 'ionicAngular.services' ]);
-
-angular.module('ionicAngular.controllers', []);
-angular.module('ionicAngular.services', []);
