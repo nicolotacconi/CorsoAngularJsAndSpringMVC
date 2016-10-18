45e07cded5b1336ec9f18f36a795d9e21be32077
Giuseppe <giuseppe.giordano1992@gmail.com>
2016-10-18 19:29:04 +0200

    esempio 12_watcher

--- CorsoAngularJsAndSpringMVC/src/main/webapp/pages/12_watcher/submit.js
+++ CorsoAngularJsAndSpringMVC/src/main/webapp/pages/12_watcher/submit.js
@@ -2,6 +2,7 @@
 angular.module('corsoBaseAngular.controllers')
 
 
+
 .controller('SubmitCtrl',['$scope', function($scope){
 	$scope.title = 'Iscriviti';
 	$scope.iscritto = false;
@@ -17,6 +18,13 @@ angular.module('corsoBaseAngular.controllers')
 		$scope.email = 'john.doe@gmail.com';
 		$scope.iscritto = false;
 	};
+	$scope.$watch('user',   //  <- posso usare il nome della variabile perché dichiaro il mio watcher dentro lo scope
+		      function tracciaCambioValore(newVal, oldVal) {// VIENE PASSATO IL VECCHIO PARAMETRO E ANCHE IL NUOVO
+		        if (newVal) {// SE IL NUOVO VALORE NON E' BLANK SCRIVO SULLA CONSOLE E AGGIORNO LE VARIABILI
+		          console.log('valore cambiato da: ' + oldVal + ' a:'  + newVal);
+		        }
+		      }
+	);
 }])
 
 
